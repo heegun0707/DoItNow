d92cc31095516b1039e8f2c93ba6494695b82b4e
leem559 <leem559@naver.com>
2025-02-27 16:41:47 +0900

    feat: TextFeild만 사용한 InputText 컴포넌트 추가

--- app/src/main/java/org/choleemduo/doitnow/ui/components/CustomInputText.kt
+++ app/src/main/java/org/choleemduo/doitnow/ui/components/CustomInputText.kt
@@ -10,10 +10,14 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
@@ -23,6 +27,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import org.choleemduo.doitnow.ui.theme.DoItNowTheme
@@ -94,7 +99,11 @@ fun ErrorInputText(
         modifier = modifier
     )
     {
-        Box(modifier = Modifier.height(43.dp).fillMaxWidth()) {
+        Box(
+            modifier = Modifier
+                .height(43.dp)
+                .fillMaxWidth()
+        ) {
             BasicTextField(
                 value = text,
                 onValueChange = onTextChange,
@@ -175,46 +184,104 @@ fun ButtonInputText(
     }
 }
 
+@Composable
+fun BaseInputText(
+    titleString: String = "",
+    hintString: String = "",
+    text: String = "",
+    textSize: TextUnit = 12.sp,
+    errorText: String? = null,
+    onTextChange: (String) -> Unit,
+    modifier: Modifier,
+    isError: Boolean = false,
+    isEnabled: Boolean = true,
+    maxLines: Int = 1
+) {
+    val colorScheme = MaterialTheme.colorScheme
+
+    Column(modifier = modifier) {
+        if (titleString.isNotEmpty()) {
+            Text(
+                text = titleString,
+                modifier = Modifier
+                    .padding(top = 1.dp, start = 1.dp),
+                color = if (isEnabled) colorScheme.onSecondary else colorScheme.tertiary
+            )
+        }
+        TextField(
+            value = text,
+            onValueChange = onTextChange,
+            modifier = Modifier
+                .fillMaxWidth(),
+            textStyle = TextStyle(fontSize = textSize),
+            enabled = isEnabled,
+            placeholder = {
+                Text(
+                    text = hintString,
+                    style = TextStyle(fontSize = textSize, color = colorScheme.tertiary)
+                )
+            },
+            isError = isError,
+            minLines = 1,
+            maxLines = maxLines,
+            singleLine = maxLines == 1,
+            colors = TextFieldDefaults.colors(
+                focusedIndicatorColor = colorScheme.primary,
+                focusedContainerColor = Color.Transparent,
+                unfocusedIndicatorColor = colorScheme.onSecondary,
+                unfocusedContainerColor = Color.Transparent,
+                errorTextColor = colorScheme.error,
+                errorSupportingTextColor = colorScheme.error,
+                errorIndicatorColor = colorScheme.error,
+                errorContainerColor = Color.Transparent,
+                disabledTextColor = colorScheme.tertiary,
+                disabledContainerColor = Color.Transparent,
+                disabledIndicatorColor = colorScheme.tertiary
+            )
+        )
+        errorText?.let {
+            if (isError) {
+                Text(
+                    text = it,
+                    modifier = Modifier.padding(top = 8.dp, start = 10.dp),
+                    fontSize = 10.sp,
+                    color = MaterialTheme.colorScheme.error
+                )
+            }
+        }
+
+    }
+
+
+}
+
 @Preview
 @Composable
 fun InputTextPreviews() {
     DoItNowTheme {
         var text by rememberSaveable { mutableStateOf("") }
-        var error by rememberSaveable { mutableStateOf(true) }
+        var error by rememberSaveable { mutableStateOf(false) }
+        var enabled by rememberSaveable { mutableStateOf(true) }
         Box(
             Modifier
                 .size(width = 412.dp, height = 917.dp)
                 .background(Color.White)
         ) {
-            ErrorInputText(
-                hintString = "일정 타이틀을 작성해주세요.",
-                text = text,
-                errorText = "에러 메세지 입니다",
-                modifier = Modifier
-                    .size(width = 363.dp, height = 77.dp)
-                    .align(Alignment.Center),
-                onTextChange = { text = it },
-                error = error
-            )
-            ButtonInputText(
+            BaseInputText(
                 titleString = "타이틀",
                 hintString = "타이틀을 입력해주세요.",
                 text = text,
+                textSize = 16.sp,
                 modifier = Modifier
-                    .size(width = 363.dp, height = 71.dp)
+                    .width(width = 363.dp)
+                    .wrapContentHeight()
                     .offset(y = 100.dp)
                     .align(Alignment.TopCenter),
-                onClick = {}
-            )
-            DefaultInputText(
-                titleString = "타이틀",
-                hintString = "타이틀을 입력해주세요.",
-                text = text,
-                modifier = Modifier
-                    .size(width = 363.dp, height = 71.dp)
-                    .offset(y = (-100).dp)
-                    .align(Alignment.BottomCenter),
-                onTextChange = { text = it }
+                onTextChange = { text = it },
+                errorText = "에러 메세지 입니다",
+                isEnabled = enabled,
+                isError = error,
+                maxLines = 3
             )
         }
     }
