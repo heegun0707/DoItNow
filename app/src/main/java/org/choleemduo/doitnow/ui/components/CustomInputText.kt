cd724c30e3c7c4c5ee94af04a6193f3b0c79d3ea
leem559 <leem559@naver.com>
2025-03-04 13:54:00 +0900

    chore: 중복되는 CustomInputText 삭제

--- app/src/main/java/org/choleemduo/doitnow/ui/components/CustomInputText.kt
+++ app/src/main/java/org/choleemduo/doitnow/ui/components/CustomInputText.kt
@@ -6,13 +6,11 @@ import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -22,125 +20,13 @@ import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import org.choleemduo.doitnow.ui.theme.DoItNowTheme
-
-@Composable
-fun DefaultInputText(
-    titleString: String = "",
-    hintString: String = "",
-    text: String = "",
-    onTextChange: (String) -> Unit,
-    modifier: Modifier
-) {
-    var isFocused by remember { mutableStateOf(false) }
-    val colorScheme = MaterialTheme.colorScheme
-
-    Box(
-        modifier = modifier
-    ) {
-        Text(
-            text = titleString,
-            modifier = Modifier
-                .align(Alignment.TopStart)
-                .padding(1.dp)
-        )
-        BasicTextField(
-            value = text,
-            onValueChange = onTextChange,
-            textStyle = TextStyle(fontSize = 12.sp, color = colorScheme.onSecondary),
-            cursorBrush = SolidColor(colorScheme.onSecondary),
-            modifier = Modifier
-                .align(Alignment.BottomStart)
-                .padding(start = 16.dp, bottom = 16.dp)
-                .onFocusChanged { focusState -> isFocused = focusState.isFocused },
-            decorationBox = { innerTextField ->
-                Box(
-                    modifier = Modifier.fillMaxWidth()
-                ) {
-                    if (text.isEmpty()) {
-                        Text(
-                            text = hintString,
-                            style = TextStyle(fontSize = 12.sp, color = colorScheme.tertiary)
-                        )
-                    }
-                    innerTextField()
-                }
-            }
-        )
-        HorizontalDivider(
-            color = colorScheme.run { if (isFocused) primary else onSecondary },
-            thickness = 1.dp,
-            modifier = Modifier.align(Alignment.BottomStart)
-        )
-    }
-}
-
-@Composable
-fun ErrorInputText(
-    hintString: String = "",
-    text: String = "",
-    errorText: String = "",
-    onTextChange: (String) -> Unit,
-    modifier: Modifier,
-    error: Boolean = false
-) {
-    var isFocused by remember { mutableStateOf(false) }
-    val colorScheme = MaterialTheme.colorScheme
-
-    Column(
-        modifier = modifier
-    )
-    {
-        Box(
-            modifier = Modifier
-                .height(43.dp)
-                .fillMaxWidth()
-        ) {
-            BasicTextField(
-                value = text,
-                onValueChange = onTextChange,
-                textStyle = TextStyle(fontSize = 16.sp, color = colorScheme.onSecondary),
-                cursorBrush = SolidColor(colorScheme.onSecondary),
-                modifier = Modifier
-                    .align(Alignment.CenterStart)
-                    .padding(start = 16.dp)
-                    .onFocusChanged { focusState -> isFocused = focusState.isFocused },
-                decorationBox = { innerTextField ->
-                    if (text.isEmpty()) {
-                        Text(
-                            text = hintString,
-                            style = TextStyle(fontSize = 16.sp, color = colorScheme.tertiary)
-                        )
-                    }
-                    innerTextField()
-                }
-            )
-        }
-        HorizontalDivider(
-            color = colorScheme.run { if (isFocused) primary else onSecondary },
-            thickness = 1.dp,
-            modifier = Modifier.align(Alignment.Start)
-        )
-        if (error && text.isEmpty()) {
-            Text(
-                text = errorText,
-                modifier = Modifier
-                    .align(Alignment.Start)
-                    .padding(start = 14.dp, top = 8.dp),
-                fontSize = 10.sp,
-                color = MaterialTheme.colorScheme.error
-            )
-        }
-    }
-}
 
 @Composable
 fun ButtonInputText(
